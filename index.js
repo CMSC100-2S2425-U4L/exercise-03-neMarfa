b86d6ed67daf385d448c4dbb3eb352e2343f6869
neMarfa <nemarfa@up.edu.ph>
2025-02-19 11:27:06 +0800

    docs: added proper documentation to the program

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+
+// function checks whether or not the password is valid
 function validatePassword(firstPassword, secondPassword){
     var alphabet = "abcdefghijklmnopqrstuvwxyz"
     var numbers = "0123456789"
     var counter = 0;
 
+    // checks if they are the same password
     if(firstPassword != secondPassword) return false;
 
+    // checks if the length is greater than or equal to 8
     if(firstPassword < 8) return false
 
+    // checks if there is atleast one lowercase letter
     for(let i = 0; i < firstPassword.length; i++){
         if(alphabet.includes(firstPassword[i])){
             counter++;
@@ -15,7 +20,9 @@ function validatePassword(firstPassword, secondPassword){
 
     if(counter == 0) return false;
     counter = 0;
+    //
 
+    // checks if there is atleast one uppercase letter
     for(let i = 0; i < firstPassword.length; i++){
         if(alphabet.toUpperCase().includes(firstPassword[i])){
             counter++;
@@ -24,7 +31,9 @@ function validatePassword(firstPassword, secondPassword){
 
     if(counter == 0) return false;
     counter = 0;
+    //
 
+    // checks if there is atleast one number
     for(let i = 0; i < firstPassword.length; i++){
         if(numbers.includes(firstPassword[i])){
             counter++;
@@ -33,25 +42,34 @@ function validatePassword(firstPassword, secondPassword){
 
     if(counter == 0) return false;
     counter = 0;
+    //
 
+    // if all conditions are satisfied it returns true
     return true;
 }
 
+// function reverses a valid password
 function reversePasscode(passcode){
     var newPasscode = ''
+    // traverses original password in reverse order and concatenates those to a new string
     for(let i = (passcode.length - 1); i >= 0; i--){
         newPasscode = newPasscode+passcode[i];
     }
     return newPasscode;
 }
 
+// responsible for storing password in an object
 function storePassword(name, passcode1, passcode2){
     var newPasscode = '';
+
+    // case when password is invalid
     if(!validatePassword(passcode1, passcode2))
         newPasscode = passcode1;
+    // case when password is valid
     else 
         newPasscode = reversePasscode(passcode1);
     
+    // note that the if statements only affect the password's value
     var profile ={
         name:name,
         newPassword: newPasscode,
